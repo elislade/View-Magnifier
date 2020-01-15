82d2415be4fb4fb79e20995f178c4ac31618359d
Eli Slade <elislade@gmail.com>
2020-01-15 15:43:37 -0800

    Initial Commit

---
+++ View Magnifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  View Magnifier
+//
+//  Created by Eli Slade on 2020-01-15.
+//  Copyright © 2020 Eli Slade. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
